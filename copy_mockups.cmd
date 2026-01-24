59c86be9dc71dc70cf0b2cb289413bb7a9f16b05
nimo <nimonit1@gmail.com>
2026-01-25 02:16:01 +0900

    gitignore update

--- copy_mockups.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-mkdir mockups
-copy "C:\Users\jun_-\.gemini\antigravity\brain\fd860012-297b-4a23-82fd-3ec8343289a7\portal_page_mockup_1769273188701.png" "mockups\portal_mockup.png"
-copy "C:\Users\jun_-\.gemini\antigravity\brain\fd860012-297b-4a23-82fd-3ec8343289a7\genre_dashboard_mockup_1769273207628.png" "mockups\dashboard_mockup.png"
-dir mockups
